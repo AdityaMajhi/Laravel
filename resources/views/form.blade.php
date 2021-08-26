cb5d438ff9c67930026048f222a235e3c3b9febc
AdityaMajhi <aditya.majhi@sifal.deerwalk.edu.np>
2021-08-26 15:06:32 +0545

    first commit

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -5,7 +5,7 @@
 </head>
 
 <body>
-
+<!-- 
 <div class="registration-form">
     <form method="post" class="form" >
         <div class="Fullname">
@@ -27,7 +27,49 @@
             <input type="submit" value="submit">
         </div>
 
+    </form> -->
+<h1>Register</h1>
+    <div class="form-group">
+    <form method="post"  class="form">
+        
+        <div class="mb-3">
+            <label class="form-label">First Number</label>
+            <input class="form-control" type="number" name="first">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Second Number</label>
+            <input class="form-control" type="number" name="second">
+        </div>
+        <label>Address</label>
+            <input class="address" type="text" >
+            <br>
+            <label>Contact no</label>
+            <input class="contact" type="number" >
+            <br>
+            <label>E-mail address</label>
+            <input class="email" type="email">
+        <input class="btn btn-primary btn-small" type="submit" value="submit">
+<!-- 
+        <form method="post" class="form" >
+        <div class="Fullname">
+            <label>FullName</label>
+            <input class="name" type="text" >
+            <br>
+            <label>Address</label>
+            <input class="address" type="text" >
+            <br>
+            <label>Contact no</label>
+            <input class="contact" type="number" >
+            <br>
+            <label>E-mail address</label>
+            <input class="email" type="email">
+            <br>
+            <label>Comment</label>
+            <input class="comment" type="text"  >
+            <br>
+            <input type="submit" value="submit">
     </form>
+</div> -->
 
 </div>
 </body>

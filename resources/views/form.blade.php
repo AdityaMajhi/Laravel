2495d3388c8f84772bbfeece3a95c5cdfe710197
AdityaMajhi <aditya.majhi@sifal.deerwalk.edu.np>
2021-08-26 13:50:36 +0545

    first commit

---
+++ resources/views/form.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE>
+<html>
+<head>
+    <title>form</title>
+</head>
+
+<body>
+
+<div class="registration-form">
+    <form method="post" class="form" >
+        <div class="Fullname">
+            <label>FullName</label>
+            <input class="name" type="text" placeholder="Enter your full name">
+            <br>
+            <label>Address</label>
+            <input class="address" type="text" placeholder="ENter your address">
+            <br>
+            <label>Contact no</label>
+            <input class="contact" type="number" placeholder="Enter your contact number">
+            <br>
+            <label>E-mail address</label>
+            <input class="email" type="email" placeholder="Enter your emailaddress">
+            <br>
+            <label>Comment</label>
+            <input class="comment" type="text" placeholder="Enter anything" >
+            <br>
+            <input type="submit" value="submit">
+        </div>
+
+    </form>
+
+</div>
+</body>
+</html>
